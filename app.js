f9b8cf1deb2f8d6959b87fc0643c483f80e5b625
Antonio Lega <atlcontact8@gmail.com>
2023-05-04 20:38:51 +0200

    express api req.param

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
 const express = require('express')
-const path = require('path')
 
 const app = express()
+const { products } = require('./data.js')
 
-// setup static and middleware
-app.use(express.static('./public'))
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product
+    return { id, name, image }
+  })
+  res.json(newProducts)
+})
 
-//   adding to static assets
-//   SSR
-// })
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req.params)
+  const { productID } = req.params
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  )
+
+  if (!singleProduct) {
+    return res.status(404).send('Product not found')
+  }
+
+  return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.param)
+  res.send('Hello World')
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000')
+  console.log('listening on port 5000....')
 })
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
-//app.get('/', (req, res) => {
-//   console.log('user hit the resource')
-//   res.status(200).send('Home Page')
-// })
-
-// app.get('/about', (req, res) => {
-//   res.status(200).send('About Page')
-// })
-
-// app.all('*', (req, res) => {
-//   res.status(404).send('<h1>resource not found</h1>')
-// })
