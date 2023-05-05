22997ac2352b44ada45cec9ab871dd7f077061b1
Antonio Lega <atlcontact8@gmail.com>
2023-05-06 00:51:06 +0200

    nodejs middleware project

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 const express = require('express')
 
 const app = express()
-const { products } = require('./data.js')
-
-app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
-})
-
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
-  res.json(newProducts)
-})
-
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req.params)
-  const { productID } = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-
-  if (!singleProduct) {
-    return res.status(404).send('Product not found')
-  }
-
-  return res.json(singleProduct)
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.param)
-  res.send('Hello World')
-})
 
 app.listen(5000, () => {
-  console.log('listening on port 5000....')
+  console.log('Server is listening to port 5000')
 })
